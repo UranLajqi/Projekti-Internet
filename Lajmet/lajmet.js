0345400def761542c7076abce1ff58716ccc8717
UranLajqi <58478405+UranLajqi@users.noreply.github.com>
2020-11-24 17:42:11 +0100

    Add files via upload

---
+++ Lajmet/lajmet.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    var h1 = $(".koka");  
+    h1.animate({left: '300px'}, "slow");
+    h1.animate({fontSize: '30px'}, "slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa2").click(function(){
+    $(".klasa3").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa4").click(function(){
+    $(".klasa5").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa6").click(function(){
+    $(".klasa7").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa8").click(function(){
+    $(".klasa9").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa10").click(function(){
+    $(".klasa11").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa12").click(function(){
+    $(".klasa13").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".klasa14").click(function(){
+    $(".klasa15").slideToggle("slow");
+  });
+});
+ 
+$(document).ready(function(){
+  $(".klasa16").click(function(){
+    $(".klasa17").slideToggle("slow");
+  });
+});
+ 
+$(document).ready(function(){
+  $(".klasa18").click(function(){
+    $(".klasa19").slideToggle("slow");
+  });
+});
+ 
+
+ 
